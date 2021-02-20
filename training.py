7f3dc4b1def0551133f568945e2b39cab8fc257b
Patrick Abdul-Ahad <k1739510@kingston.ac.uk>
2021-02-20 17:53:00 +0000

    I have commented all my code, amended the errors and ensured it was working. This code has used a lemmatizer to divide the sentences into words, it has iterated through the json file linked each pattern word to its tag and saved, as lists, the differrent tags, the tokonized patter words and the links between tags and words.

--- training.py
+++ training.py
@@ -1,28 +1,35 @@
-import random
+import random  # required for choosing a random response
 import json
-import pickle
-import numpy as no
+import pickle  # for serialization
+import numpy as no  # for np
 
-import nltk
+import nltk  # natural language tool kit
 from nltk.stem import WordNetLemmatizer
-
+# the lemmatizer will reduce the word to its stem. For example, work, working, worked, works is all the same stem
+# word as "work"
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optimizers import SFG
+from tensorflow.keras.optimizers import SGD
+# SGD stands for Stochastic gradient descent
 
-lemmatizer = WorldNetLemmatizer()
+lemmatizer = WordNetLemmatizer
 
-Intents = json.loads(open('intents.json').read())
+intentsDictionary = json.loads(open("intents.json").read())
 
+# created 3 empty lists and the letters this program will ignore
 words = []
 classes = []
-documents = []
+documents = []  # the belongings
 ignore_letters = ["?", "!", ",", "."]
 
-for intent in intents["intents"]:
+# I am iterating over the intents
+for intent in intentsDictionary["intents"]:
+    # for each of the patterns, the below will tokenize the sentences. Meaning the sentences will split into words.
     for pattern in intent["patterns"]:
-        word_list = nltk.word_tokenize(pattern)
-        words.extend((word_list,intent["tag"]))
+        listOfWords = nltk.word_tokenize(pattern)
+        words.extend(listOfWords)
+        documents.append((listOfWords, intent["tag"]))
+        # for each tag discovered, if not added to the classes list yet, it becomes added.
         if intent["tag"] not in classes:
             classes.append(intent["tag"])
 
